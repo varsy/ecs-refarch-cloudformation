570d0f945a48bc75494f0cb2eff20112049cbe86
Andrey Sizov <andrey.sizov@jetbrains.com>
2017-08-10 16:41:22 +0300

    moved to top

--- terraform/s3.tf
+++ terraform/s3.tf
@@ -7,48 +7,48 @@ resource "aws_s3_bucket" "cloudformation" {
 resource "aws_s3_bucket_object" "master" {
   bucket = "${aws_s3_bucket.cloudformation.bucket}"
   key = "master.yaml"
-  source = "../master.yaml"
-  etag = "${md5(file("../master.yaml"))}"
+  source = "../ecs/master.yaml"
+  etag = "${md5(file("../ecs/master.yaml"))}"
 }
 
 resource "aws_s3_bucket_object" "infra-vpc" {
   bucket = "${aws_s3_bucket.cloudformation.bucket}"
   key = "infrastructure/vpc.yaml"
-  source = "../infrastructure/vpc.yaml"
-  etag = "${md5(file("../infrastructure/vpc.yaml"))}"
+  source = "../ecs/infrastructure/vpc.yaml"
+  etag = "${md5(file("../ecs/infrastructure/vpc.yaml"))}"
 }
 
 resource "aws_s3_bucket_object" "infra-security-groups" {
   bucket = "${aws_s3_bucket.cloudformation.bucket}"
   key = "infrastructure/security-groups.yaml"
-  source = "../infrastructure/security-groups.yaml"
-  etag = "${md5(file("../infrastructure/security-groups.yaml"))}"
+  source = "../ecs/infrastructure/security-groups.yaml"
+  etag = "${md5(file("../ecs/infrastructure/security-groups.yaml"))}"
 }
 
 resource "aws_s3_bucket_object" "infra-load-balancers" {
   bucket = "${aws_s3_bucket.cloudformation.bucket}"
   key = "infrastructure/load-balancers.yaml"
-  source = "../infrastructure/load-balancers.yaml"
-  etag = "${md5(file("../infrastructure/load-balancers.yaml"))}"
+  source = "../ecs/infrastructure/load-balancers.yaml"
+  etag = "${md5(file("../ecs/infrastructure/load-balancers.yaml"))}"
 }
 
 resource "aws_s3_bucket_object" "infra-ecs-cluster" {
   bucket = "${aws_s3_bucket.cloudformation.bucket}"
   key = "infrastructure/ecs-cluster.yaml"
-  source = "../infrastructure/ecs-cluster.yaml"
-  etag = "${md5(file("../infrastructure/ecs-cluster.yaml"))}"
+  source = "../ecs/infrastructure/ecs-cluster.yaml"
+  etag = "${md5(file("../ecs/infrastructure/ecs-cluster.yaml"))}"
 }
 
 resource "aws_s3_bucket_object" "infra-rds" {
   bucket = "${aws_s3_bucket.cloudformation.bucket}"
   key = "infrastructure/rds.yaml"
-  source = "../infrastructure/rds.yaml"
-  etag = "${md5(file("../infrastructure/rds.yaml"))}"
+  source = "../ecs/infrastructure/rds.yaml"
+  etag = "${md5(file("../ecs/infrastructure/rds.yaml"))}"
 }
 
 resource "aws_s3_bucket_object" "infra-teamcity-server" {
   bucket = "${aws_s3_bucket.cloudformation.bucket}"
   key = "teamcity/server.yaml"
-  source = "../teamcity/server.yaml"
-  etag = "${md5(file("../teamcity/server.yaml"))}"
+  source = "../ecs/teamcity/server.yaml"
+  etag = "${md5(file("../ecs/teamcity/server.yaml"))}"
 }
